bd4a9db991d742475752fc656e77ea80b2fdc3fb
Mahmoud <ma7moud.awad.1@gmail.com>
2025-07-15 01:17:22 +0300

    finallllllll

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 let list = document.querySelector(".header .container ul")
+// let listTr = document.querySelector(".header .container ul.trans")
 let btn = document.querySelector(".header .container .list")
 let btnX = document.querySelector(".header .container ul i")
 let header = document.querySelector(".header")
+let li = document.querySelectorAll(".header .container ul li a")
 btn.onclick = () => {
-    list.classList.add("active")
+    list.classList.add("active");
+    list.classList.add("trans");
+    document.body.style.overflow = 'hidden';
+    
 }
 btnX.onclick = () => {
-    list.classList.remove("active")
+    list.classList.remove("trans");
+    document.body.style.overflow = 'auto';
 }
-// window.onscroll = () => {
-//     if(window.pageYOffset > 30) {
-//         header.classList.add("active")
-//     }
+
+li.forEach((ele) => {
+    ele.onclick = () => {
+        // list.classList.remove("active")
+        list.classList.remove("trans")
+        document.body.style.overflow = 'auto';
+    }
+})
+
+// if(list.classList.contains("active")) {
+    
 // }
+window.onscroll = () => {
+    if(window.pageYOffset > 50) {
+        header.classList.add("active")
+    }else{
+        header.classList.remove("active")
+    }
+}
